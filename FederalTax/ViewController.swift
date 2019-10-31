32a238569ecb577d13c98eb27f7cb60da845d4c7
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-10-31 10:09:21 -0400

    Started the outlook and did outlets

--- FederalTax/ViewController.swift
+++ FederalTax/ViewController.swift
@@ -10,10 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    //Mark: Properties
+    
+    
+    @IBOutlet weak var nameEntered: UITextField!
+    
+    @IBOutlet weak var grossAnualIncome: UITextField!
+    
+    @IBOutlet weak var errorLabelname: UILabel!
+    
+    @IBOutlet weak var errorLabelIncome: UILabel!
+    
+    
+    //Mark: Methods
+    
+    
+    //Runs as soon as the view becomes visible to the user
+     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //  Get the amount of bill value from the text field
+    @IBAction func calculateTax (sender:Any){
+        
+        //Check if user entered text in textfield
+        guard let nameEntered  = nameEntered.text, nameEnteredString != ""  else  {
+            errorLabelname.text = "Please enter a name"
+        }
+            
+        }
+    
 
 
 }
